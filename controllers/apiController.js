64eefdc1fb56ef58aa15ab5d71fe6c1f6f9fee07
Rahmad Alfian Syahputra <rahmadalfiansyahputra48@gmail.com>
2021-08-10 00:21:04 +0700

    update API

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,16 +1,28 @@
-const Materi = require("../models/Materi")
-
+const Materi = require("../models/Materi");
 
 module.exports = {
   home: async (req, res) => {
     try {
-      const materi = await Materi.find();
+      const materi = await Materi.find().select("_id title");
       res.status(200).json({
         materi,
-      })
+      });
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: "Internal server error" });
     }
-  },  
-}
+  },
+
+  detailMateri: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const detail = await Materi.findOne({ _id: id });
+      res.status(200).json({
+        detail,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
+};
--- routes/api.js
+++ routes/api.js
@@ -6,9 +6,10 @@ const apiController = require("../controllers/apiController");
 // router.get("/detail-item/:id", apiController.detailItem);
 // router.get("/select-category/:id", apiController.selectCategory);
 // router.get("/toko", apiController.toko);
-router.get("/home", apiController.home);
 // router.get("/pesanan", apiController.pesanan);
 // router.get("/detail-page/:id", apiController.detailPage);
 // router.post("/booking-page", upload, apiController.bookingPage);
+router.get("/home", apiController.home);
+router.get("/detail-materi/:id", apiController.detailMateri);
 
 module.exports = router;
